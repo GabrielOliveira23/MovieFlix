cfc8eb33a2400670f2784aa19770e4bef2af1a07
GabrielOliveira23 <gabriel_leomartins@hotmail.com>
2025-05-15 18:04:43 -0300

    feat: add configuration files and update dependencies for improved functionality

---
+++ config.ts
@@ -0,0 +1,7 @@
+const config = {
+	DATABASE_ID: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID ?? '',
+	COLLECTION_ID: process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID ?? '',
+	PROJECT_ID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID ?? '',
+};
+
+export default config;
